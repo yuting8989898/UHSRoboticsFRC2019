058c1dd0edb532223b0e05968eaafa7ea0861a11
yitiJ <32946204+rokc@users.noreply.github.com>
2019-02-16 15:12:56 -0500

    mapped all the OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -29,52 +29,51 @@ public class OI {
    * Cartesian X-Axis
    */
   public static double getDriveX() {
-    double x = mainOI.getRawAxis(2);
-    return x > Constant.joystickDeadZone || x < -Constant.joystickDeadZone ? x : 0;
+    return correctJoystick(mainOI.getRawAxis(2)); //Left joystick
   }
 
   /**
    * Cartesian Y-Axis
    */
   public static double getDriveY() {
-    double y = -mainOI.getRawAxis(1);
-    return y > Constant.joystickDeadZone || y < -Constant.joystickDeadZone ? y : 0;
+    return correctJoystick(-mainOI.getRawAxis(1)); //Right joystick
   }
 
-  //TODO map all the buttons for subOI correctly
   public static double getArm() {
-    double y = -subOI.getRawAxis(3);
-    return y > Constant.joystickDeadZone || y < -Constant.joystickDeadZone ? y : 0;
+    return correctJoystick(-subOI.getRawAxis(5)); //Right joystick
   }
 
   public static double getWrist() {
-    double y = -subOI.getRawAxis(4);
-    return y > Constant.joystickDeadZone || y < -Constant.joystickDeadZone ? y : 0;
+    double output = correctJoystick(mainOI.getRawAxis(3)); //downward (left trigger) 
+    return output - correctJoystick(mainOI.getRawAxis(4)); //upward (right trigger)
   }
 
   public static double getLift() {
-    double output = -subOI.getRawAxis(1);
-    output = output > Constant.joystickDeadZone || output < -Constant.joystickDeadZone ? output : 0;
+    double output = correctJoystick(-subOI.getRawAxis(1)); //Left joystick
     if(output == 0){
       //-1~1 is reserved for the joystick
-    if (subOI.getRawButton(1))  output = 2; //hatch loading station
-    if (subOI.getPOV()==90 )           output = 3; //cargo loading station
-    if (subOI.getRawButton(2))  output = 4; //hatch level 1
-    if (subOI.getPOV()==180)           output = 5; //cargo level 1
-    if (subOI.getRawButton(3))  output = 6; //hatch level 2
-    if (subOI.getPOV()==270)           output = 7; //cargo level 2
-    if (subOI.getRawButton(4))  output = 8; //hatch level 3
-    // if (subOI.getPOV()==0)             output = 9; //cargo level 3
+    if (subOI.getRawButton(1))  output = 2; //hatch loading station (left btn)
+    if (subOI.getPOV()==270 )           output = 3; //cargo loading station(left pov)
+    if (subOI.getRawButton(2))  output = 4; //hatch level 1 (down btn)
+    if (subOI.getPOV()==180)           output = 5; //cargo level 1 (down pov)
+    if (subOI.getRawButton(3))  output = 6; //hatch level 2 (right btn)
+    if (subOI.getPOV()==90)           output = 7; //cargo level 2 (right pov)
+    if (subOI.getRawButton(4))  output = 8; //hatch level 3 (up btn)
+    // if (subOI.getPOV()==0)             output = 9; //cargo level 3 (up pov)
     }
     SmartDashboard.putNumber("Lift Controller Value", output);
     return output;
   }
 
   public static boolean getIntakePressed(){
-    return mainOI.getRawButton(4);
+    return mainOI.getRawButton(5); //Back Left btn
   }
   
   public static boolean getRevIntakePressed(){
-    return mainOI.getRawButton(2);
+    return mainOI.getRawButton(6); //back right btn
+  }
+
+  public static double correctJoystick(double input){
+    return input > Constant.joystickDeadZone || input < -Constant.joystickDeadZone ? input : 0;
   }
 }
